e73c224aa89c62ca78fbda05e603bb4070e28d7c
diazhdez <jd5531066@gmail.com>
2025-06-15 23:11:08 -0600

    new database

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 load_dotenv()
 
 class Config:
-    DATABASE_URL = os.getenv('DATABASE_URL', 'mysql://root:TNNCAVqwzCkkTJWKwwhTjhzjKAuWDpMW@junction.proxy.rlwy.net:17227/railway')
+    DATABASE_URL = os.getenv('DATABASE_URL', 'mysql://root:HEHBtccXAwjCOWQVpuwuaeRHMYjTOBXG@yamabiko.proxy.rlwy.net:26834/railway')
     SECRET_KEY = os.getenv('SECRET_KEY', 'M0i1Xc$GfPw3Yz@2SbQ9lKpA5rJhDtE7')  # Usa una clave por defecto en caso de que no esté en .env
 
     @staticmethod
